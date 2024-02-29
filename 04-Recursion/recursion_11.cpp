cf306126e64f3c64021ee79492ec004ff1a0ad9d
RAHUL PRASAD <157337927+Rahwik@users.noreply.github.com>
2024-02-29 19:21:38 +0530

    Add files via upload

---
+++ 04-Recursion/recursion_11.cpp
@@ -0,0 +1,23 @@
+//Subsets Sum Problem
+#include<iostream>
+using namespace std;
+
+int countsubsets(int arr[], int n, int sum)
+{
+    if (n == 0)
+    {
+        return (sum == 0) ? 1 : 0;
+    }
+    return countsubsets(arr, n - 1, sum) + countsubsets(arr, n - 1, sum - arr[n - 1]);
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 10;
+
+    cout << "Number of subsets with sum " << sum << ": " << countsubsets(arr, n, sum) << endl;
+
+    return 0;
+}
