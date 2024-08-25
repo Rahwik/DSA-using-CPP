ba3756af5c440254d54a98a64dba6d23fe9d2b9d
Rahul <rahul.3057.12@outlook.com>
2024-08-25 17:31:41 +0530

    new problem

--- 10_Strings/09_strings.cpp
+++ 10_Strings/09_strings.cpp
@@ -1,51 +1,71 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 //Overview of pattern searching
 //Naive Pattern Searching
-void patSearching(string &txt,string &pat)
+void patSearching(string &txt, string &pat)
 {
-    int m=pat.length();
-    int n=txt.length();
-    for(int i=0;i<=n-m;i++)
+    int m = pat.length();
+    int n = txt.length();
+    for (int i = 0; i <= n - m; i++)
     {
         int j;
-        for(j=0;j<m;j++)
+        for (j = 0; j < m; j++)
         {
-            if(pat[j]!=txt[i+j])
+            if (pat[j] != txt[i + j])
             {
                 break;
             }
         }
-        if(j==m)
+        if (j == m)
         {
-            cout<<i<<" ";
+            cout << i << " ";
         }
     }
 }
+
 //Improved Naive Algorithm for Distinct (in pattern)
 //shifts after mismatch is encountered
-void patSearch(string &str,string &pat)
+void patSearch(string &txt, string &pat)
 {
-    int n=txt.length();
-    int m=pat.length();
-    for(int i=0;i<=n-m;i++)
+    int n = txt.length();
+    int m = pat.length();
+    for (int i = 0; i <= n - m; i++)
     {
         int j;
-        for(j=0;j<m;j++)
+        for (j = 0; j < m; j++)
         {
-            if(pat[j]!=txt[i+j])
-                {
-                    break;
-                }
+            if (pat[j] != txt[i + j])
+            {
+                break;
+            }
         }
-        if(j==m)
+        if (j == m)
         {
-            cout<<i<<" ";
+            cout << i << " ";
         }
-        if(j==)
+        if (j == 0)
         {
             i++;
         }
-        else{
-            i=(i+j);
+        else
+        {
+            i = i + j;
         }
     }
+}
+
+int main()
+{
+    string txt, pat;
+    cout << "Enter the text: ";
+    getline(cin, txt);
+    cout << "Enter the pattern: ";
+    getline(cin, pat);
+    cout << "Naive Pattern Searching: ";
+    patSearching(txt, pat);
+    cout << "\nImproved Naive Pattern Searching: ";
+    patSearch(txt, pat);
+    return 0;
 }
