d30e59f9555eae6ada006828dc5f911d7f43470b
Rahul <rahul.3057.12@outlook.com>
2024-08-30 14:14:31 +0530

    Dequeue

---
+++ 14_Deque/01_deque.cpp
@@ -0,0 +1,17 @@
+//Deque : 
+/*
+operations operformed  in deqeue :
+~insertfront()
+~insertrear()
+~deletefront()
+~deleterear()
+~getfront()
+~getrear()
+~isfull()
+~size()
+
+Implementaion :
+~linked list : Doubly linked list with front and rear pointer in C++ .
+~Array : Circular array with front and rear indexes.
+         we can use front a size and get rear using size.
+*/
