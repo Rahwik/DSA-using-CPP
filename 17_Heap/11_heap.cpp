ed3d25a8829ead084c54359cdac7c0e07daf85fd
Rahul <rahul.3057.12@outlook.com>
2024-09-02 08:55:12 +0530

    heap

--- 17_Heap/11_heap.cpp
+++ 17_Heap/11_heap.cpp
@@ -1,1 +1,45 @@
-//Merge K Sorted Arrays
+// Merge K Sorted Arrays
+#include <queue>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> mergeKSortedArrays(vector<vector<int>>& arrays) {
+        vector<int> result;
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> minHeap;
+
+        for (int i = 0; i < arrays.size(); i++) {
+            if (!arrays[i].empty()) {
+                minHeap.push({arrays[i][0], {i, 0}});
+            }
+        }
+
+        while (!minHeap.empty()) {
+            auto top = minHeap.top();
+            minHeap.pop();
+            result.push_back(top.first);
+
+            if (top.second.second + 1 < arrays[top.second.first].size()) {
+                minHeap.push({arrays[top.second.first][top.second.second + 1], {top.second.first, top.second.second + 1}});
+            }
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<vector<int>> arrays = {{1, 3, 5}, {2, 4, 6}, {0, 7, 8}};
+    vector<int> result = solution.mergeKSortedArrays(arrays);
+
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;  
+
+    return 0;
+}
--- 17_Heap/12_heap.cpp
+++ 17_Heap/12_heap.cpp
@@ -1,1 +1,46 @@
-//Median of a Stream
+// Median of a Stream
+#include<iostream>
+#include<queue>
+using namespace std;
+
+void printM(int arr[], int n) {
+    priority_queue<int> s; 
+    priority_queue<int, vector<int>, greater<int>> g;
+
+    s.push(arr[0]);
+    cout << arr[0] << endl;
+
+    for (int i = 1; i < n; i++) {
+        int x = arr[i];
+
+        if (s.size() > g.size()) {
+            if (s.top() > x) {
+                s.push(x);
+            } else {
+                g.push(x);
+                if (g.size() > s.size()) {
+                    s.push(g.top());
+                    g.pop();
+                }
+            }
+        } else {
+            if (x <= s.top()) {
+                s.push(x);
+            } else {
+                g.push(x);
+            }
+        }
+
+        if (s.size() == g.size()) {
+            cout << (s.top() + g.top()) / 2.0 << endl;
+        } else {
+            cout << s.top() << endl;
+        }
+    }
+}
+int main() {
+    int arr[] = {12, 15, -10, 0, 10, 13, 5, 23, 25};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printM(arr, n);
+    return 0;
+}
